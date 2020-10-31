b286f40cc1a2dd37a23134e99d3f020721d2efd5
fallah9721160031ali <linux22push@gmail.com>
2020-10-31 11:02:04 +0330

    Realtime Add Comment

--- src/Post.js
+++ src/Post.js
@@ -2,16 +2,32 @@ import React from 'react';
 import Faker from 'faker';
 import { render } from 'react-dom';
 import Timelast from './Timelast';
+import Comments from './Comments';
 
 class Post extends React.Component{
     constructor(props){
         super(props);
         this.avatarimgsrc = Faker.internet.avatar();
+        this.username = Faker.internet.userName();
+        this.textPost = Faker.lorem.sentences();
         this.state = {
-            
+            commnets: [<Comments/>]
         };
     }
 
+
+    sendComment = () =>{
+        if (document.getElementById("myCommentText").value == ''){
+            alert("Write a Comment before Send it :/");
+            return;
+        }
+
+        this.state.commnets.push(<Comments comment={document.getElementById("myCommentText").value} />)
+        this.setState(
+            {
+                commnets:this.state.commnets ,
+            });
+    }
     render(){
         return (
             <div className="Post">
@@ -22,7 +38,7 @@ class Post extends React.Component{
             <div className="Post-detail-head">
                 <img className="profileimage" src={this.avatarimgsrc}/>
                 <div className="Post-detail-head-username">
-                    {Faker.internet.userName()}
+                    {this.username}
                     <span style={{marginLeft:'6px'}}>•</span>
                     <a href="#"><span style={{marginLeft:'6px'}}>Following</span></a>
                 </div>
@@ -32,30 +48,13 @@ class Post extends React.Component{
                 <div className="Post-detail-content-usertext">
                 <img className="profileimage" src={this.avatarimgsrc}/>
                 <div className="Post-detail-text">
-                    {Faker.internet.userName()}{' '}
-                    <span>{Faker.lorem.sentences()}</span>
+                    {this.username}{' '}
+                    <span>{this.textPost}</span>
                     <p className="timelasted">3h</p>
                 </div>
                 </div>
                 <div className="post-detail-content-comments">
-                <div className="post-detail-content-comment">
-                    <div className="post-detail-content-comment-header">
-                    <img className="profileimage" src={Faker.internet.avatar()}/>
-                    <div className="Post-detail-text">
-                        {Faker.internet.userName()}{' '}
-                        <span>{Faker.lorem.sentence()}</span>
-                    </div>
-                    { Faker.random.boolean() ?
-                    <svg aria-label="Like" id="comment-like" fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
-                    : <svg aria-label="Unlike" id="comment-like" fill="#ed4956" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
-                    }
-                    </div>
-                    <div className="comment-detail">
-                    <p style={{fontWeight:"normal"}}>1m</p>
-                    <p>2 like</p>
-                    <p>Replay</p>
-                    </div>
-                </div>
+                    {this.state.commnets}
                 </div>
             </div>
             <div className="Post-detail-footer">
@@ -80,8 +79,8 @@ class Post extends React.Component{
             </div>
 
             <div className="Post-detail-send-comment">
-                <input className="commentfield" type="text" placeholder="Add a comment..."></input>
-                <input className="sendcommandbtn" type="submit" value="Post"></input>
+                <input id="myCommentText" className="commentfield" type="text" placeholder="Add a comment..."></input>
+                <input className="sendcommandbtn" type="submit" onClick={this.sendComment} value="Post"></input>
             </div>
             </div>
         </div>
